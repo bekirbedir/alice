c08fd94fffe25d434bb3b226d9d537bc938b4612
Bekir Bedir <bekirbedir25@gmail.com>
2021-06-18 22:44:56 +0300

    tar,h duzeltme

--- alice-ui/src/app/activity-management/activity-management/activity-management.component.ts
+++ alice-ui/src/app/activity-management/activity-management/activity-management.component.ts
@@ -48,14 +48,14 @@ export class ActivityManagementComponent implements OnInit {
 
   ngOnInit(): void {
     this.tr = {
-      firstDayOfWeek: 0,
-      dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
-      dayNamesShort: ["Paz", "Pzt", "Sal", "ÇRŞ", "PRŞ", "CMA", "CTS"],
-      dayNamesMin: ["PA", "PT", "SA", "ÇA", "PE", "CU", "PZ"],
-      monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-      monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-      today: 'Today',
-      clear: 'Clear',
+      firstDayOfWeek: 1,
+      dayNames: [ "Pazar","Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+      dayNamesShort: ["Pzr","Pzt", "Sal", "Çrş", "Prş", "Cma", "Cts"],
+      dayNamesMin: ["PZ","PT", "SA", "ÇA", "PE", "CU", "CT"],
+      monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+      monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz", "Temm", "Agu", "Eyl", "Eki", "Kas", "Ara"],
+      today: 'Bugün',
+      clear: 'Temizle',
       dateFormat: 'dd.mm.yy',
       weekHeader: 'Wk'
     };
